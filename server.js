40feae114d05fc4d8e0b2c47c9d66b4bcd10af76
Louis Boehling <louisboehling@gmail.com>
2018-05-07 11:09:54 -0400

    logic

--- server.js
+++ server.js
@@ -14,7 +14,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-//
+// Set Handlebars as the view engine
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
